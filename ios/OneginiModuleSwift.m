ce47d504a4e37f23a17c08f1a796975e7fb8773b
Krystian Rusek <krystian.rusek@sigma.se>
2020-11-30 14:04:55 +0100

    copy android Milestone 2/3.

---
+++ ios/OneginiModuleSwift.m
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+
+@interface RCT_EXTERN_MODULE(OneginiModuleSwift, RCTEventEmitter)
+RCT_EXTERN_METHOD(supportedEvents)
+RCT_EXTERN_METHOD(startClient:(RCTResponseSenderBlock *)callback)
+RCT_EXTERN_METHOD(getRedirectUri:(RCTResponseSenderBlock *)callback)
+RCT_EXTERN_METHOD(registerUser:(NSString *)identityProviderId callback:(RCTResponseSenderBlock *)callback)
+RCT_EXTERN_METHOD(handleRegistrationCallback:(NSString *)url)
+RCT_EXTERN_METHOD(cancelRegistration)
+RCT_EXTERN_METHOD(submitPinAction:(NSString *)action isCreatePinFlow:(nonnull NSNumber *)isCreatePinFlow pin:(NSString *)pin)
+RCT_EXTERN_METHOD(logout:(RCTResponseSenderBlock *)callback)
+@end
