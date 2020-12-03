939eff9176a2c57209ffaf44148ad8819463ef2e
valeriy.rumak <valeriy.rumak@youshido.com>
2020-12-03 19:51:34 +0200

    iOS: correct files for module to work

--- ios/RNOneginiSdk.m
+++ ios/RNOneginiSdk.m
@@ -3,7 +3,7 @@
 #import <React/RCTEventEmitter.h>
 
 
-@interface RCT_EXTERN_MODULE(OneginiModuleSwift, RCTEventEmitter)
+@interface RCT_EXTERN_MODULE(RNOneginiSdk, RCTEventEmitter)
 RCT_EXTERN_METHOD(supportedEvents)
 RCT_EXTERN_METHOD(startClient:(RCTResponseSenderBlock *)callback)
 RCT_EXTERN_METHOD(getRedirectUri:(RCTResponseSenderBlock *)callback)
@@ -11,5 +11,4 @@ RCT_EXTERN_METHOD(registerUser:(NSString *)identityProviderId callback:(RCTRespo
 RCT_EXTERN_METHOD(handleRegistrationCallback:(NSString *)url)
 RCT_EXTERN_METHOD(cancelRegistration)
 RCT_EXTERN_METHOD(submitPinAction:(NSString *)action isCreatePinFlow:(nonnull NSNumber *)isCreatePinFlow pin:(NSString *)pin)
-RCT_EXTERN_METHOD(logout:(RCTResponseSenderBlock *)callback)
 @end
