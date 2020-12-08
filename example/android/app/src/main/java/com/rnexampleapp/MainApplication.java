d71f9a13177125b6fba07158791a3d11b31a9320
valeriy.rumak <valeriy.rumak@youshido.com>
2020-12-08 12:06:08 +0200

    module refs

--- example/android/app/src/main/java/com/rnexampleapp/MainApplication.java
+++ example/android/app/src/main/java/com/rnexampleapp/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.onegini.mobile.OneginiModulePackage;
+import com.onegini.mobile.RNOneginiSdkPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
 
-          packages.add(new OneginiModulePackage());
+          packages.add(new RNOneginiSdkPackage());
           return packages;
         }
 
