656a58976ba0e833dbf6edc0a6796129328ebd77
valeriy.rumak <valeriy.rumak@youshido.com>
2020-12-09 22:09:06 +0200

    fixed auth errors

--- ios/ErrorMappings/AuthenticationErrorDomainMapping.swift
+++ ios/ErrorMappings/AuthenticationErrorDomainMapping.swift
@@ -1,40 +1,40 @@
 class AuthenticationErrorDomainMapping {
     let title = "Authentication error"
 
-    func mapErrorWithPinChallenge(pinChallenge: ONGPinChallenge) -> AppError {
+    func mapErrorWithPinChallenge(pinChallenge: ONGPinChallenge) -> SdkError {
         let remainingFailureCount = String(describing: pinChallenge.remainingFailureCount)
         let errorDescription = "PIN you've entered is invalid."
         let recoverySuggestion = "You have still \(remainingFailureCount) attempts left."
-        return AppError(title: title, errorDescription: errorDescription, recoverySuggestion: recoverySuggestion)
+        return SdkError(title: title, errorDescription: errorDescription, recoverySuggestion: recoverySuggestion)
     }
 
-    func mapErrorWithCustomInfo(_ customInfo: ONGCustomInfo) -> AppError {
+    func mapErrorWithCustomInfo(_ customInfo: ONGCustomInfo) -> SdkError {
         if customInfo.status >= 4000 && customInfo.status < 5000 {
             let message = "Authentication failed"
-            return AppError(title: title, errorDescription: message)
+            return SdkError(title: title, errorDescription: message)
         } else {
-            return AppError(errorDescription: "Something went wrong.")
+            return SdkError(errorDescription: "Something went wrong.")
         }
     }
 
-    func mapError(_ error: Error) -> AppError {
+    func mapError(_ error: Error) -> SdkError {
         switch error.code {
         case ONGAuthenticationError.authenticatorDeregistered.rawValue:
             let message = "The Authenticator has been deregistered."
             let recoverySuggestion = "Please register used authenticator and try again."
-            return AppError(title: title, errorDescription: message, recoverySuggestion: recoverySuggestion)
+            return SdkError(title: title, errorDescription: message, recoverySuggestion: recoverySuggestion)
 
         case ONGAuthenticationError.authenticatorInvalid.rawValue:
             let message = "The authenticator that you provided is invalid."
             let recoverySuggestion = "It may not exist, please verify whether you have supplied the correct authenticator."
-            return AppError(title: title, errorDescription: message, recoverySuggestion: recoverySuggestion)
+            return SdkError(title: title, errorDescription: message, recoverySuggestion: recoverySuggestion)
 
         case ONGAuthenticationError.touchIDAuthenticatorFailure.rawValue:
             let message = "Authentication with the biometric authenticator has failed."
-            return AppError(title: title, errorDescription: message)
+            return SdkError(title: title, errorDescription: message)
 
         default:
-            return AppError(errorDescription: "Something went wrong.")
+            return SdkError(errorDescription: "Something went wrong.")
         }
     }
 }
