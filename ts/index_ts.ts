a2408eefa290fa3e2ec4cf3f69138ba86caca81e
Ufos <>
2021-03-23 15:02:04 +0100

    Refactor - just cast NativeModule to provided interface

--- ts/index_ts.ts
+++ ts/index_ts.ts
@@ -1,5 +1,4 @@
 // TODO: rename as index.ts (main index)
-// TODO: separate modules? Start, Resources, ...
 // TODO: test and unify errors (plain string)
 
 import {
@@ -8,6 +7,10 @@ import {
   NativeEventEmitter,
   DeviceEventEmitter,
 } from 'react-native';
+import * as Types from './data-types';
+
+//
+
 const {RNOneginiSdk} = NativeModules;
 
 // helpers
@@ -16,141 +19,73 @@ const isAndroid = Platform.OS === 'android';
 
 //
 
-namespace OneginiSdkTs {
-  //
-  // Start
-  //
+interface SDK {
+  // Setup
+  startClient(sdkConfig: Types.Config): Promise<string | null>;
 
-  export interface CustomProvider {
-    id: string;
-    isTwoStep: boolean;
-  }
+  // Data getters
+  getIdentityProviders(): Promise<Types.IdentityProvider>;
+  getAccessToken(): Promise<string>;
+  getAuthenticatedUserProfile(): Promise<Types.Profile>;
+  getUserProfiles(): Promise<Types.Profile[]>;
+  getRedirectUri(): Promise<Types.RedirectUri>; // TODO: I think it should be moved "behind" SDK - dev should not know about it
 
-  export interface Config {
-    enableFingerprint: boolean;
-    securityControllerClassName: string;
-    enableMobileAuthenticationOtp: boolean;
-    configModelClassName: string | null;
-    customProviders: [CustomProvider?];
-  }
-
-  const DefaultConfig: Config = {
-    enableFingerprint: true,
-    securityControllerClassName: 'com.rnexampleapp.SecurityController',
-    enableMobileAuthenticationOtp: true,
-    customProviders: [{id: '2-way-otp-api', isTwoStep: true}],
-    configModelClassName: null,
-  };
-
-  // Promise returns error msg?
-  export function startClient(
-    sdkConfig: Config = DefaultConfig,
-  ): Promise<string | null> {
-    return isIOS
-      ? RNOneginiSdk.startClient() // why there is no default config on iOS?
-      : RNOneginiSdk.startClient(sdkConfig);
-  }
-
-  //
-  // Resources
-  //
-
-  export interface IdentityProvider {
-    id: string;
-    name: string;
-  }
-
-  export interface Profile {
-    profileId: string;
-  }
-
-  export interface ImplicitUserDetails {
-    decoratedUserId: string;
-  }
-
-  export interface RedirectUri {
-    redirectUri: string;
-  }
-
-  export interface AppDetailsResources {
-    applicationIdentifier: string;
-    applicationPlatform: string;
-    applicationVersion: string;
-  }
-
-  export interface Device {
-    id: string;
-    name: string;
-    application: string;
-    platform: string;
-    isMobileAuthenticationEnabled: boolean;
-  }
-
-  //
-
-  export function getIdentityProviders(): Promise<IdentityProvider> {
-    return RNOneginiSdk.getIdentityProviders();
-  }
-
-  // TODO: use it in ExampleApp
-  // Does it return string or object map?
-  export function getAccessToken(): Promise<string> {
-    return RNOneginiSdk.getAccessToken();
-  }
-
-  // TODO: use it in ExampleApp
-  export function getAuthenticatedUserProfile(): Promise<Profile> {
-    return RNOneginiSdk.getAuthenticatedUserProfile();
-  }
-
-  export function getUserProfiles(): Promise<Profile[]> {
-    return RNOneginiSdk.getUserProfiles();
-  }
-
-  // TODO: I think it should be moved "behind" SDK - dev should not know about it
-  export function getRedirectUri(): Promise<RedirectUri> {
-    return RNOneginiSdk.getRedirectUri();
-  }
-
-  export function getImplicitDataResource(
+  // Resource getters
+  getImplicitDataResource(
     profileId: string,
-  ): Promise<ImplicitUserDetails> {
-    return RNOneginiSdk.getImplicitDataResource(profileId);
-  }
+  ): Promise<Types.ImplicitUserDetails>;
+  getAppDetailsResource(): Promise<Types.AppDetailsResources>;
+  getDeviceListResource(): Promise<Types.Device[]>;
 
-  export function getAppDetailsResource(): Promise<AppDetailsResources> {
-    return RNOneginiSdk.getAppDetailsResource();
-  }
-
-  export function getDeviceListResource(): Promise<Device[]> {
-    return RNOneginiSdk.getDeviceListResource();
-  }
-
-  //
   // User register/deregister
-  //
+  registerUser(identityProviderId: string): Promise<Types.Profile>;
+  deregisterUser(profileId: string): Promise<any>;
+  handleRegistrationCallback(uri: string): void; // TODO: I think it should be moved "behind" SDK - dev should not know about it
+  cancelRegistration(): void;
 
-  export function registerUser(identityProviderId: string): Promise<Profile> {
-    //@todo return whole user + pass userName from RN
-    return RNOneginiSdk.registerUser(identityProviderId);
-  }
-
-  export function deregisterUser(profileId: string): Promise<any> {
-    return RNOneginiSdk.deregisterUser(profileId);
-  }
-
-  // TODO: I think it should be moved "behind" SDK - dev should not know about it
-  export function handleRegistrationCallback(uri: string) {
-    RNOneginiSdk.handleRegistrationCallback(uri);
-  }
-
-  export function cancelRegistration() {
-    RNOneginiSdk.cancelRegistration();
-  }
-
-  //
-  //
-  //
+  // Authentication
+  authenticateUser(profileId: string): Promise<Types.AuthData>;
+  logout(): Promise<any>; // any or void when we have null from native?
+  getAllAuthenticators(profileId: string): Promise<Types.Authenticator[]>; // TODO: use it in ExampleApp
+  getRegisteredAuthenticators(
+    profileId: string,
+  ): Promise<Types.Authenticator[]>;
+  setPreferredAuthenticator(
+    profileId: string,
+    idOneginiAuthenticator: string,
+  ): Promise<any>; // TODO: check this path and check if resolve is called on Native side
 }
+
+//
+
+export const DefaultConfig: Types.Config = {
+  enableFingerprint: true,
+  securityControllerClassName: 'com.rnexampleapp.SecurityController',
+  enableMobileAuthenticationOtp: true,
+  customProviders: [{id: '2-way-otp-api', isTwoStep: true}],
+  configModelClassName: null,
+};
+
+//
+
+const OneginiSdkTs: SDK = {
+  ...(RNOneginiSdk as SDK),
+  //
+  // override methods if needed
+  //
+  startClient: (
+    sdkConfig: Types.Config = DefaultConfig,
+  ): Promise<string | null> => {
+    console.log('START CLIENT!');
+
+    if (isIOS) {
+      return RNOneginiSdk.startClient();
+    } else {
+      return RNOneginiSdk.startClient(sdkConfig);
+    }
+  },
+};
+
+//
 
 export default OneginiSdkTs;
