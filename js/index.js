a5b65411dc975d65ca4b80c9a5698d583f0fc18c
valeriy.rumak <valeriy.rumak@youshido.com>
2021-03-30 18:33:07 +0300

    js: removed old methods(resources related) comment

--- js/index.js
+++ js/index.js
@@ -76,11 +76,6 @@ const OneginiSdk = {
   authenticateDeviceForResource: (resourcePath) => RNOneginiSdk.authenticateDeviceForResource(resourcePath),
   resourceRequest: (isImplicit = false, details = {}) => RNOneginiSdk.resourceRequest(isImplicit, details),
 
-  // this was replaced by methods above
-  // getImplicitDataResource: (profileId) => RNOneginiSdk.getImplicitDataResource(profileId),
-  // getAppDetailsResource: () => RNOneginiSdk.getAppDetailsResource(),
-  // getDeviceListResource: () => RNOneginiSdk.getDeviceListResource(),
-
   // User register/deregister
   registerUser: (identityProvider) => RNOneginiSdk.registerUser(identityProvider), //@todo return whole user + pass userName from RN
   deregisterUser: (profileId) => RNOneginiSdk.deregisterUser(profileId),
