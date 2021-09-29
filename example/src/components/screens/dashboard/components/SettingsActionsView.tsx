7cf1b9d2921ecdd031d543e91b0cc0f175135d6d
Ufos <>
2021-09-29 12:54:55 +0200

    Dont show error if it was cancel

--- example/src/components/screens/dashboard/components/SettingsActionsView.tsx
+++ example/src/components/screens/dashboard/components/SettingsActionsView.tsx
@@ -9,7 +9,14 @@ const onChangePinPressed = () => {
   //@todo handle deregistration error when codes will be presented
   OneginiSdk.changePin()
     .then(() => Alert.alert('Success'))
-    .catch((error) => Alert.alert('error', JSON.stringify(error)));
+    .catch((error) => {
+      if (error) {
+        // this is cancel
+        if (error.code !== '9006') {
+          Alert.alert('error', JSON.stringify(error));
+        }
+      }
+    });
 };
 
 const renderButton = (
