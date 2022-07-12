b0d26ff39b4ee33b5eccb696fc2c35ec0d8c147a
Michiel Vrins <michiel_678@hotmail.com>
2022-07-12 15:34:23 +0200

    RNP-67: Fetch profiles on error during authorization
    Previously when authorization failed it would not remove the profile
    that got deregistered due to the failed attempt. It will now fetch the
    profiles again when it encounters any kind of error. This could also be
    when a profile doesn't exist on the token server forinstance.

--- example/src/components/screens/auth/AuthScreen.tsx
+++ example/src/components/screens/auth/AuthScreen.tsx
@@ -40,6 +40,7 @@ const AuthScreen: React.FC<Props> = (props) => {
             props.onAuthorized?.(true);
         } catch (e: any) {
             setError(e.message);
+            fetchProfiles();
         }
     }, []);
 
