0f1a88439ceaee0dcc247c5133c0abaffa38e09d
Michiel Vrins <michiel_678@hotmail.com>
2022-07-14 09:54:46 +0200

    RNP-67: Refactor AuthScreen and AuthButton to not duplicate logic
    We had logic for authenticating in both the AuthButton component and in
    the AuthScreen component. This logic in AuthScreen was used when
    multiple profiles were added and the logic in AuthButton when a single
    profile was added. The logic was also not the exact same and was
    buggy for the AuthButton. This change moves all the logic from the
    AuthScreen into the AuthButton component.

--- example/src/components/screens/auth/AuthScreen.tsx
+++ example/src/components/screens/auth/AuthScreen.tsx
@@ -1,114 +1,41 @@
-import React, {useCallback, useContext, useEffect, useRef, useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import {Assets} from '../../../../assets';
 import RegisterButton from './components/RegisterButton';
 import AuthButton from './components/AuthButton';
 import Button from '../../general/Button';
 import InfoView from '../info/InfoView';
-import OneWelcomeSdk from "onewelcome-react-native-sdk";
-import {CurrentUser} from "../../../auth/auth";
-import ModalSelector from "react-native-modal-selector";
-import {AuthContext} from "../../../providers/auth.provider";
-import {AuthActionTypes} from "../../../providers/auth.actions";
 
 interface Props {
-    onAuthorized?: (success?: boolean) => void;
+  onAuthorized?: (success?: boolean) => void;
 }
 
 const AuthScreen: React.FC<Props> = (props) => {
-    const {
-        state: {authenticated: {loading, profiles}},
-        dispatch,
-    } = useContext(AuthContext);
     const [isInfoVisible, setIsInfoVisible] = useState(false);
-    const [error, setError] = useState('');
-    const isModalOpen = useRef(false);
-
-    const authenticateProfile = useCallback(async (id: string) => {
-        try {
-            const authenticated = await OneWelcomeSdk.authenticateUser(id);
-            if (!authenticated) return;
-            CurrentUser.id = id;
-
-            props.onAuthorized?.(true);
-        } catch (e: any) {
-            setError(e.message);
-            fetchProfiles();
-        }
-    }, []);
-
-    const fetchProfiles = async () => {
-        try {
-            dispatch({type: AuthActionTypes.AUTH_LOAD_PROFILE_IDS});
-            const userProfiles = await OneWelcomeSdk.getUserProfiles();
-            dispatch({
-                type: AuthActionTypes.AUTH_SET_PROFILE_IDS,
-                payload: userProfiles?.map(({profileId}) => profileId) || []
-            });
-        } catch (e: any) {
-            setError(e.message);
-            dispatch({type: AuthActionTypes.AUTH_SET_PROFILE_IDS, payload: []});
-        }
-    };
-
-    useEffect(() => {
-        if (!loading) {
-            fetchProfiles();
-        }
-    }, []);
-
-    useEffect(() => {
-        if (!profiles && !loading) {
-            fetchProfiles();
-        }
-    }, [profiles, loading, fetchProfiles]);
 
     return isInfoVisible ? (
         <InfoView onFinished={() => setIsInfoVisible(false)}/>
     ) : (
         <View style={styles.container}>
-        <View style={styles.logoHolder}>
-            <Image source={Assets.logo}/>
-            <Text style={styles.logoText}>Example App</Text>
-        </View>
-        <Text style={styles.errorText}>{error}</Text>
+            <View style={styles.logoHolder}>
+                <Image source={Assets.logo}/>
+                <Text style={styles.logoText}>Example App</Text>
+            </View>
+            
+            <AuthButton onAuthorized={props.onAuthorized}/>
 
-        {profiles && profiles.length > 1 ? (
-            <ModalSelector
-                data={profiles.map(p => p.id)}
-                initValue={profiles[0].id}
-                selectedKey={profiles[0].id}
-                keyExtractor={(item) => item}
-                labelExtractor={(item) => item}
-                selectedItemTextStyle={{fontWeight: '700'}}
-                onModalClose={() => {
-                    isModalOpen.current = false;
-                }}
-                onModalOpen={() => {
-                    isModalOpen.current = true;
-                }}
-                onChange={(option) => {
-                    if (isModalOpen.current) {
-                        authenticateProfile(option);
-                    }
-                }}>
-
-                <Button name='LOG IN WITH ...'>Log in</Button>
-            </ModalSelector>) : (<AuthButton onAuthorized={props.onAuthorized}/>)
-        }
-
-        <View style={styles.registerContainer}>
-            <RegisterButton onRegistered={props.onAuthorized}/>
+            <View style={styles.registerContainer}>
+                <RegisterButton onRegistered={props.onAuthorized}/>
+            </View>
+            <View style={styles.infoContainer}>
+                <Button
+                    name="INFO"
+                    onPress={() => {
+                        setIsInfoVisible(true);
+                    }}
+                />
+            </View>
         </View>
-        <View style={styles.infoContainer}>
-            <Button
-                name={'INFO'}
-                onPress={() => {
-                    setIsInfoVisible(true);
-                }}
-            />
-        </View>
-    </View>
     );
 };
 
