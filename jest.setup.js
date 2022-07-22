6cc3635e3f912d25cc20424ede07da456bcd0bd5
Michiel Vrins <michiel_678@hotmail.com>
2022-07-22 11:31:33 +0200

    Add mocks for jest to allow it to use @react-native-async-storage

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,7 @@
 import {NativeModules} from 'react-native';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 NativeModules.RNOneginiSdk = {
   startClient: jest.fn(),
