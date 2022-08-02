8211a5f2577cb7873232d0678f6cfefda64c23ea
Michiel Vrins <michiel_678@hotmail.com>
2022-08-02 14:44:56 +0200

    RNP-85: Add events types for the different events of the sdk
    Previously we just had a list of enums for the different kind of events
    without any coherence between these events. This commit adds types for
    these events so that users can see what kind of events can be fired.
    
    These events are usable with switch statements and will automatically
    infer the correct type when you select them on their 'action' property.

--- ts/events.ts
+++ ts/events.ts
@@ -1,3 +1,127 @@
+export type SdkNotificationEvent =
+  | PinNotificationEvent
+  | CustomRegistrationNotificationEvent
+  | FingerprintNotificationEvent
+  | MobileAuthOtpNotificationEvent;
+
+// PinNotification
+export type PinNotificationEvent =
+  | PinNotificationCloseEvent
+  | PinNotificationChangedEvent
+  | PinNotificationConfirmEvent
+  | PinNotificationOpenEvent
+  | PinNotificationErrorEvent;
+
+export type PinNotificationCloseEvent = {
+  action: PinNotification.Close;
+  flow: PinFlow;
+};
+
+export type PinNotificationChangedEvent = {
+  action: PinNotification.Changed;
+  flow: PinFlow;
+};
+
+// TODO: Remove this, it is only here so it compiles but will be removed when the SDK/example app remove this event.
+export type PinNotificationConfirmEvent = {
+  action: PinNotification.Confirm;
+  flow: PinFlow;
+};
+
+export type PinNotificationOpenEvent = {
+  action: PinNotification.Open;
+  flow: PinFlow;
+  profileId: string;
+  data?: number; //pin length
+};
+
+export type PinNotificationErrorEvent = {
+  action: PinNotification.Error;
+  flow: PinFlow;
+  errorType: number;
+  errorMsg: string;
+  userInfo?: {
+    remainingFailureCount: string;
+  };
+};
+
+//CustomRegistrationNotification
+export type CustomRegistrationNotificationEvent =
+  | initRegistrationEvent
+  | finishRegistrationEvent;
+
+export type initRegistrationEvent = {
+  action: CustomRegistrationNotification;
+  identityProviderId: string;
+  customInfo?: {
+    data: string;
+    status: number;
+  };
+};
+
+export type finishRegistrationEvent = {
+  action: CustomRegistrationNotification;
+  identityProviderId: string;
+  customInfo?: {
+    data: string;
+    status: number;
+  };
+};
+
+//MobileAuthOtpNotification
+export type MobileAuthOtpNotificationEvent =
+  | startOtpAuthenticationEvent
+  | finishOtpAuthenticationEvent;
+
+export type startOtpAuthenticationEvent = {
+  action: MobileAuthOtpNotification.StartAuthentication;
+  mobileAuthenticationRequest: {
+    message: string;
+    type: string;
+    transactionId: string;
+    signingData: string;
+  };
+};
+
+export type finishOtpAuthenticationEvent = {
+  action: MobileAuthOtpNotification.FinishAuthentication;
+  mobileAuthenticationRequest: {
+    message: string;
+    type: string;
+    transactionId: string;
+    signingData: string;
+  };
+};
+
+//FingerPrintNotification
+export type FingerprintNotificationEvent =
+  | startFingerprintAuthenticationEvent
+  | onNextFingerprintAuthenticationAttemptEvent
+  | onFingerprintCapturedEvent
+  | finishFingerprintAuthenticationEvent;
+export type startFingerprintAuthenticationEvent = {
+  action: FingerprintNotification.StartAuthentication;
+  userProfile: {
+    profileId: string;
+  };
+};
+export type onNextFingerprintAuthenticationAttemptEvent = {
+  action: FingerprintNotification.OnNextAuthenticationAttempt;
+};
+export type onFingerprintCapturedEvent = {
+  action: FingerprintNotification.OnFingerprintCaptured;
+};
+export type finishFingerprintAuthenticationEvent = {
+  action: FingerprintNotification.FinishAuthentication;
+};
+
+export enum SdkNotification {
+  Pin = 'ONEWELCOME_PIN_NOTIFICATION',
+  CustomRegistration = 'ONEWELCOME_CUSTOM_REGISTRATION_NOTIFICATION',
+  MobileAuthOtp = 'ONEWELCOME_MOBILE_AUTH_OTP_NOTIFICATION',
+  Fingerprint = 'ONEWELCOME_FINGERPRINT_NOTIFICATION',
+}
+
 export enum PinAction {
   ProvidePin = 'provide',
   Cancel = 'cancel',
@@ -14,13 +138,7 @@ export enum PinNotification {
   Confirm = 'confirm',
   Close = 'close',
   Error = 'show_error',
-}
-
-export enum SdkNotification {
-  Pin = 'ONEWELCOME_PIN_NOTIFICATION',
-  CustomRegistration = 'ONEWELCOME_CUSTOM_REGISTRATION_NOTIFICATION',
-  MobileAuthOtp = 'ONEWELCOME_MOBILE_AUTH_OTP_NOTIFICATION',
-  Fingerprint = 'ONEWELCOME_FINGERPRINT_NOTIFICATION',
+  Changed = 'changed',
 }
 
 export enum CustomRegistrationAction {
