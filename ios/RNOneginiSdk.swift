6f73d36bfef4770690aee6dede8e06c79dacce4e
Michiel Vrins <michiel_678@hotmail.com>
2022-10-19 16:42:53 +0200

    RNP-100: TS/iOS: update logout signature/resolve value
    TS: Update signature to promise<void>
    iOS: change resolve true to nil

--- ios/RNOneginiSdk.swift
+++ ios/RNOneginiSdk.swift
@@ -266,7 +266,7 @@ class RNOneginiSdk: RCTEventEmitter, ConnectorToRNBridgeProtocol {
             if let error = error {
                 reject("\(error.code)", error.localizedDescription, error)
               } else {
-                resolve(true)
+                resolve(nil)
               }
         }
     }
