2e3ffd680b19df8b46d626f84a0360a34729e8ad
Michiel Vrins <michiel_678@hotmail.com>
2022-10-19 16:20:35 +0200

    RNP-100: iOS/TS: update signature/return value of deregisterUser
    iOS: return nil instead of true
    TS: change signature to Promise<void>

--- ios/RNOneginiSdk.swift
+++ ios/RNOneginiSdk.swift
@@ -165,7 +165,7 @@ class RNOneginiSdk: RCTEventEmitter, ConnectorToRNBridgeProtocol {
             if let error = error {
                 reject("\(error.code)", error.localizedDescription, error)
               } else {
-                resolve(true)
+                resolve(nil)
               }
         }
     }
