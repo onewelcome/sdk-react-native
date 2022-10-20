bc671f553c14a3db560c7c335dd6bfc5a8785925
Michiel Vrins <michiel_678@hotmail.com>
2022-10-20 11:45:58 +0200

    RNP-100: iOS: Update handleRegistrationCallback
    iOS was not rejecting when no registration was in progress. This commit
    adds that functionality. It also does a small refactor for how
    cancelRegistration is handled internally.

--- ios/RNOneginiSdk.swift
+++ ios/RNOneginiSdk.swift
@@ -177,8 +177,11 @@ class RNOneginiSdk: RCTEventEmitter, ConnectorToRNBridgeProtocol {
             return
         }
 
-        bridgeConnector.toRegistrationConnector.registrationHandler.processRedirectURL(url: urlOBject)
-        resolve(nil)
+        if (bridgeConnector.toRegistrationConnector.registrationHandler.processRedirectURL(url: urlOBject)) {
+            resolve(nil)
+        } else {
+            reject(String(WrapperError.registrationNotInProgress.code), WrapperError.registrationNotInProgress.description, WrapperError.registrationNotInProgress)
+        }
     }
 
     @objc
