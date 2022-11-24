7157e96115480bcab8b954cd0e47b53120140e4c
Michiel Vrins <michiel_678@hotmail.com>
2022-11-24 16:48:54 +0100

    RNP-111: Use uri facade in sso usecase

--- android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/StartSingleSignOnUseCase.kt
+++ android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/StartSingleSignOnUseCase.kt
@@ -1,21 +1,21 @@
 package com.onegini.mobile.sdk.reactnative.clean.use_cases
 
-import android.net.Uri
 import com.facebook.react.bridge.Promise
 import com.onegini.mobile.sdk.android.handlers.OneginiAppToWebSingleSignOnHandler
 import com.onegini.mobile.sdk.android.handlers.error.OneginiAppToWebSingleSignOnError
 import com.onegini.mobile.sdk.android.model.OneginiAppToWebSingleSignOn
 import com.onegini.mobile.sdk.reactnative.OneginiSDK
+import com.onegini.mobile.sdk.reactnative.facade.UriFacade
 import com.onegini.mobile.sdk.reactnative.mapers.OneginiAppToWebSingleSignOnMapper
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class StartSingleSignOnUseCase @Inject constructor(private val oneginiSDK: OneginiSDK) {
+class StartSingleSignOnUseCase @Inject constructor(private val oneginiSDK: OneginiSDK, private val uriFacade: UriFacade) {
 
     operator fun invoke (uri: String, promise: Promise) {
         oneginiSDK.oneginiClient.userClient.getAppToWebSingleSignOn(
-            Uri.parse(uri),
+            uriFacade.parse(uri),
             object : OneginiAppToWebSingleSignOnHandler {
                 override fun onSuccess(oneginiAppToWebSingleSignOn: OneginiAppToWebSingleSignOn) {
                     promise.resolve(OneginiAppToWebSingleSignOnMapper.toWritableMap(oneginiAppToWebSingleSignOn))
