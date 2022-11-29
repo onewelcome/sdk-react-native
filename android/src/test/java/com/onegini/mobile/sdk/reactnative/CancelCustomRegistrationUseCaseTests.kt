e3f8f47f169cf3b10a739677088db7654be103c0
Michiel Vrins <michiel_678@hotmail.com>
2022-11-29 11:10:07 +0100

    RNP-114: Add tests for cancelCustomRegistrationUseCase

--- android/src/test/java/com/onegini/mobile/sdk/reactnative/CancelCustomRegistrationUseCaseTests.kt
+++ android/src/test/java/com/onegini/mobile/sdk/reactnative/CancelCustomRegistrationUseCaseTests.kt
@@ -1,11 +1,11 @@
 package com.onegini.mobile.sdk.reactnative
 
-import android.net.Uri
 import com.facebook.react.bridge.Promise
-import com.onegini.mobile.sdk.android.handlers.request.callback.OneginiBrowserRegistrationCallback
+import com.onegini.mobile.sdk.android.handlers.request.callback.OneginiCustomRegistrationCallback
 import com.onegini.mobile.sdk.reactnative.clean.use_cases.CancelCustomRegistrationUseCase
 import com.onegini.mobile.sdk.reactnative.exception.CANCEL_CUSTOM_REGISTRATION_NOT_ALLOWED
 import com.onegini.mobile.sdk.reactnative.exception.OneginiWrapperErrors.ACTION_NOT_ALLOWED
+import com.onegini.mobile.sdk.reactnative.handlers.customregistration.CustomRegistrationEventEmitter
 import com.onegini.mobile.sdk.reactnative.handlers.customregistration.SimpleCustomRegistrationAction
 import com.onegini.mobile.sdk.reactnative.handlers.customregistration.SimpleCustomRegistrationFactory
 import com.onegini.mobile.sdk.reactnative.handlers.registration.RegistrationEventEmitter
@@ -16,7 +16,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Answers
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
 import org.mockito.kotlin.spy
 import org.mockito.kotlin.verify
 
@@ -30,13 +32,13 @@ class CancelCustomRegistrationUseCaseTests {
     lateinit var promiseMock: Promise
 
     @Mock
-    lateinit var uri: Uri
-
-    @Mock
     lateinit var registrationEventEmitter: RegistrationEventEmitter
 
     @Mock
-    lateinit var oneginiBrowserRegistrationCallback: OneginiBrowserRegistrationCallback
+    lateinit var customRegistrationEventEmitter: CustomRegistrationEventEmitter
+
+    @Mock
+    lateinit var oneginiCustomRegistrationCallback: OneginiCustomRegistrationCallback
 
     lateinit var cancelCustomRegistrationUseCase: CancelCustomRegistrationUseCase
 
@@ -46,12 +48,15 @@ class CancelCustomRegistrationUseCaseTests {
 
 
     private val cancelMessage = "cancel message"
+    private val successMessage = "success message"
 
     @Before
     fun setup() {
+        simpleCustomRegistrationFactory = SimpleCustomRegistrationFactory(customRegistrationEventEmitter)
         registrationRequestHandler = RegistrationRequestHandler(registrationEventEmitter)
         cancelCustomRegistrationUseCase = CancelCustomRegistrationUseCase(oneginiSdk)
     }
+
 
     @Test
     fun `When registration has not started should reject with ACTION_NOT_ALLOWED`() {
@@ -59,27 +64,61 @@ class CancelCustomRegistrationUseCaseTests {
         verify(promiseMock).reject(ACTION_NOT_ALLOWED.code.toString(), CANCEL_CUSTOM_REGISTRATION_NOT_ALLOWED)
     }
 
+    // ONE STEP
     @Test
-    fun `When registration has started but pin creation has also started should reject with ACTION_NOT_ALLOWED`() {
-        whenRegistrationStarted()
+    fun `One Step - When registration has started but pin creation has also started should reject with ACTION_NOT_ALLOWED`() {
+        whenRegistrationStarted(false)
         whenPinCreationStarted()
         cancelCustomRegistrationUseCase(cancelMessage, promiseMock)
         verify(promiseMock).reject(ACTION_NOT_ALLOWED.code.toString(), CANCEL_CUSTOM_REGISTRATION_NOT_ALLOWED)
     }
 
     @Test
-    fun `When registration has started and pin creation has not started should resolve with null`() {
-        whenRegistrationStarted()
+    fun `One Step - When registration has started and pin creation has not started should resolve with null`() {
+        whenRegistrationStarted(false)
         cancelCustomRegistrationUseCase(cancelMessage, promiseMock)
         verify(promiseMock).resolve(null)
     }
 
-    private fun whenRegistrationStarted() {
-        registrationRequestHandler.startRegistration(uri, oneginiBrowserRegistrationCallback)
+    @Test
+    fun `One Step - When registration has started and pin creation has not started should call returnError on the action`() {
+        whenRegistrationStarted(false)
+        cancelCustomRegistrationUseCase(cancelMessage, promiseMock)
+        verify(oneginiSdk.simpleCustomRegistrationActions.first()).returnError(any())
+    }
+
+    // TWO STEP
+    @Test
+    fun `Two Step - When registration has started but pin creation has also started should reject with ACTION_NOT_ALLOWED`() {
+        whenRegistrationStarted(true)
+        whenPinCreationStarted()
+        cancelCustomRegistrationUseCase(cancelMessage, promiseMock)
+        verify(promiseMock).reject(ACTION_NOT_ALLOWED.code.toString(), CANCEL_CUSTOM_REGISTRATION_NOT_ALLOWED)
+    }
+
+    @Test
+    fun `Two Step - When registration has started and pin creation has not started should resolve with null`() {
+        whenRegistrationStarted(true)
+        cancelCustomRegistrationUseCase(cancelMessage, promiseMock)
+        verify(promiseMock).resolve(null)
+    }
+
+    @Test
+    fun `Two Step - When registration has started and pin creation has not started should call returnError on the action`() {
+        whenRegistrationStarted(true)
+        cancelCustomRegistrationUseCase(cancelMessage, promiseMock)
+        verify(oneginiSdk.simpleCustomRegistrationActions.first()).returnError(any())
+    }
+
+
+    private fun whenRegistrationStarted(isTwoStep: Boolean) {
+        // finishRegistration is what is called by the SDK when it starts registration
+        whenIdentityProviderExists(isTwoStep)
+        oneginiSdk.simpleCustomRegistrationActions.first().finishRegistration(oneginiCustomRegistrationCallback, null)
     }
 
     private fun whenPinCreationStarted() {
-        registrationRequestHandler.handleRegistrationCallback(null)
+        oneginiSdk.simpleCustomRegistrationActions.first().returnSuccess(successMessage)
     }
 
 
@@ -89,10 +128,5 @@ class CancelCustomRegistrationUseCaseTests {
         val list = ArrayList<SimpleCustomRegistrationAction>()
         list.add(customRegistrationAction)
         `when`(oneginiSdk.simpleCustomRegistrationActions).thenReturn(list)
-    }
-
-    private fun whenRegistrationIsInProgress() {
-        // finishRegistration is what is called by the SDK when it starts registration
-        oneginiSdk.simpleCustomRegistrationActions.first().finishRegistration(oneginiCustomRegistrationCallback, null)
     }
 }
