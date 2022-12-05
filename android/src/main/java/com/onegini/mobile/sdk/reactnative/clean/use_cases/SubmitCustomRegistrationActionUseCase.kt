81f569435686538c6d6af76521f10ffac1402184
Michiel Vrins <michiel_678@hotmail.com>
2022-12-05 13:12:09 +0100

    RNP-112: Refactor SubmitCustomRegistrationActionUseCase

--- android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/SubmitCustomRegistrationActionUseCase.kt
+++ android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/SubmitCustomRegistrationActionUseCase.kt
@@ -5,6 +5,7 @@ import com.onegini.mobile.sdk.reactnative.exception.OneginiReactNativeException
 import com.onegini.mobile.sdk.reactnative.exception.OneginiWrapperErrors.ACTION_NOT_ALLOWED
 import com.onegini.mobile.sdk.reactnative.exception.OneginiWrapperErrors.IDENTITY_PROVIDER_NOT_FOUND
 import com.onegini.mobile.sdk.reactnative.exception.SUBMIT_CUSTOM_REGISTRATION_ACTION_NOT_ALLOWED
+import com.onegini.mobile.sdk.reactnative.handlers.customregistration.SimpleCustomRegistrationAction
 import com.onegini.mobile.sdk.reactnative.managers.RegistrationManager
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -14,14 +15,20 @@ class SubmitCustomRegistrationActionUseCase @Inject constructor(private val regi
     operator fun invoke(identityProviderId: String, token: String?, promise: Promise) {
         when (val action = registrationManager.getSimpleCustomRegistrationAction(identityProviderId)) {
             null -> return promise.reject(IDENTITY_PROVIDER_NOT_FOUND.code.toString(), IDENTITY_PROVIDER_NOT_FOUND.message)
-            else -> {
-                try {
-                    action.returnSuccess(token)
-                    promise.resolve(null)
-                } catch (exception: OneginiReactNativeException) {
-                    promise.reject(ACTION_NOT_ALLOWED.code.toString(), SUBMIT_CUSTOM_REGISTRATION_ACTION_NOT_ALLOWED)
-                }
-            }
+            else -> tryReturnSuccess(action, token, promise)
+        }
+    }
+
+    private fun tryReturnSuccess(
+        action: SimpleCustomRegistrationAction,
+        token: String?,
+        promise: Promise
+    ) {
+        try {
+            action.returnSuccess(token)
+            promise.resolve(null)
+        } catch (exception: OneginiReactNativeException) {
+            promise.reject(ACTION_NOT_ALLOWED.code.toString(), SUBMIT_CUSTOM_REGISTRATION_ACTION_NOT_ALLOWED)
         }
     }
 }
