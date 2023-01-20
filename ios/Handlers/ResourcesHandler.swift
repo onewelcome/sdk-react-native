356902e392407fa766397c4092a7bfb37fe3eda8
Michiel Vrins <michiel_678@hotmail.com>
2023-01-20 13:15:04 +0100

    RNP-139: Don't use seperate methods for the resource calls
    Move resourceCalls to swift api.
    
    +
    
    Previously we had 3 separate private functions with lots of duplicate
    code. This commit combines all of the different calls into 1 method as
    they all use the same request object anyway.

--- ios/Handlers/ResourcesHandler.swift
+++ ios/Handlers/ResourcesHandler.swift
@@ -18,69 +18,46 @@ enum ResourceRequestType: String {
 }
 
 class ResourceHandler: BridgeToResourceHandlerProtocol {
+    private var deviceClient: DeviceClient {
+        return SharedDeviceClient.instance
+    }
+    
+    private var userClient: UserClient {
+        return SharedUserClient.instance
+    }
+    
     func resourceRequest(_ type: ResourceRequestType, _ details: NSDictionary, _ completion: @escaping (String?, Error?) -> Void) {
         guard let path = details["path"] as? String else {
             let error = NSError(domain: ONGFetchImplicitResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "'path' can not be empty"])
             completion(nil, error)
             return
         }
-        guard let method = details["method"] as? String else {
+        guard let methodName = details["method"] as? String, let method = HTTPMethod(rawValue: methodName.uppercased()) else {
             let error = NSError(domain: ONGFetchImplicitResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "'method' can not be empty"])
             completion(nil, error)
             return
         }
-        let request = ONGResourceRequest(path: path, method: method, parameters: details["parameters"] as? [String : Any], encoding: ONGParametersEncoding.formURL, headers: details["headers"] as? [String : String]);
+
+        let request = ResourceRequestFactory.makeResourceRequest(path: path, method: method, parameters: details["parameters"] as? [String: Any], body: nil, headers: details["headers"] as? [String: String], parametersEncoding: .formURL)
+        
+        let completionHandler = { (_ response: ResourceResponse?,_ error: Error?) in
+            if let error = error {
+                completion(nil, error)
+            } else {
+                if let data = response?.data {
+                    completion(String(data: data, encoding: .utf8), nil)
+                } else {
+                    let error = NSError(domain: ONGFetchResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Response doesn't contain data."])
+                    completion(nil, error)
+                }
+            }
+        }
+        
         switch(type) {
-        case .anonymous: anonymousResourcesRequest(request, completion);
-        case .implicitUser: implicitResourcesRequest(request, completion);
-        case .user: userResourcesRequest(request, completion);
-        }
-    }
-
-
-
-    fileprivate func userResourcesRequest(_ request: ONGResourceRequest, _ completion: @escaping (String?, Error?) -> Void) {
-        ONGUserClient.sharedInstance().fetchResource(request) { response, error in
-            if let error = error {
-                completion(nil, error)
-            } else {
-                if let data = response?.data {
-                    completion(String(data: data, encoding: .utf8), nil)
-                } else {
-                    let error = NSError(domain: ONGFetchResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Response doesn't contain data."])
-                    completion(nil, error)
-                }
-            }
-        }
-    }
-    
-    fileprivate func anonymousResourcesRequest(_ request: ONGResourceRequest, _ completion: @escaping (String?, Error?) -> Void) {
-        ONGDeviceClient.sharedInstance().fetchResource(request) { response, error in
-            if let error = error {
-                completion(nil, error)
-            } else {
-                if let data = response?.data {
-                    completion(String(data: data, encoding: .utf8), nil)
-                } else {
-                    let error = NSError(domain: ONGFetchResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Response doesn't contain data."])
-                    completion(nil, error)
-                }
-            }
-        }
-    }
-
-    fileprivate func implicitResourcesRequest(_ request: ONGResourceRequest, _ completion: @escaping (String?, Error?) -> Void) {
-        ONGUserClient.sharedInstance().fetchImplicitResource(request) { response, error in
-            if let error = error {
-                completion(nil, error)
-            } else {
-                if let data = response?.data {
-                    completion(String(data: data, encoding: .utf8), nil)
-                } else {
-                    let error = NSError(domain: ONGFetchImplicitResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Response doesn't contain data."])
-                    completion(nil, error)
-                }
-            }
+        // It is unclear if deviceClient.sendRequest is the correct method to call, but the deviceClient.sendUnauthenticatedRequest method does NOT seem to work, so let's use this one instead which appears to work.
+        case .anonymous: deviceClient.sendRequest(request, completion: completionHandler)
+        case .implicitUser: userClient.sendImplicitRequest(request, completion: completionHandler)
+        case .user: userClient.sendAuthenticatedRequest(request, completion: completionHandler)
         }
     }
 }
