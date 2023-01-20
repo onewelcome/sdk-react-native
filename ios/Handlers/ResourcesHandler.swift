e5f9783745df48887cfb87d31287bb8228b93c85
Michiel Vrins <michiel_678@hotmail.com>
2023-01-20 14:07:25 +0100

    RNP-139: iOS: ResourceRequest: Extract functionality to separate method

--- ios/Handlers/ResourcesHandler.swift
+++ ios/Handlers/ResourcesHandler.swift
@@ -27,37 +27,40 @@ class ResourceHandler: BridgeToResourceHandlerProtocol {
     }
     
     func resourceRequest(_ type: ResourceRequestType, _ details: NSDictionary, _ completion: @escaping (String?, Error?) -> Void) {
-        guard let path = details["path"] as? String else {
-            let error = NSError(domain: ONGFetchImplicitResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "'path' can not be empty"])
-            completion(nil, error)
-            return
-        }
-        guard let methodName = details["method"] as? String, let method = HTTPMethod(rawValue: methodName.uppercased()) else {
-            let error = NSError(domain: ONGFetchImplicitResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "'method' can not be empty"])
-            completion(nil, error)
-            return
-        }
 
-        let request = ResourceRequestFactory.makeResourceRequest(path: path, method: method, parameters: details["parameters"] as? [String: Any], body: nil, headers: details["headers"] as? [String: String], parametersEncoding: .formURL)
-        
         let completionHandler = { (_ response: ResourceResponse?,_ error: Error?) in
             if let error = error {
                 completion(nil, error)
             } else {
-                if let data = response?.data {
-                    completion(String(data: data, encoding: .utf8), nil)
-                } else {
-                    let error = NSError(domain: ONGFetchResourceErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Response doesn't contain data."])
-                    completion(nil, error)
-                }
+                completion(String(data: response?.data ?? Data.init(), encoding: .utf8), nil)
             }
         }
         
-        switch(type) {
-        // It is unclear if deviceClient.sendRequest is the correct method to call, but the deviceClient.sendUnauthenticatedRequest method does NOT seem to work, so let's use this one instead which appears to work.
-        case .anonymous: deviceClient.sendRequest(request, completion: completionHandler)
-        case .implicitUser: userClient.sendImplicitRequest(request, completion: completionHandler)
-        case .user: userClient.sendAuthenticatedRequest(request, completion: completionHandler)
+        do {
+            let request = try buildRequest(details)
+            switch(type) {
+            case .anonymous: deviceClient.sendRequest(request, completion: completionHandler)
+            case .implicitUser: userClient.sendImplicitRequest(request, completion: completionHandler)
+            case .user: userClient.sendAuthenticatedRequest(request, completion: completionHandler)
+            }
+        } catch {
+            completion(nil, error)
+            return
         }
+    }
+    
+    private func buildRequest(_ details: NSDictionary) throws -> ResourceRequest {
+        guard let path = details["path"] as? String else {
+            throw WrapperError.parametersNotCorrect(description: "'path' can not be empty")
+        }
+        guard let methodName = details["method"] as? String,
+              let method = HTTPMethod(rawValue: methodName.uppercased())
+        else {
+            throw WrapperError.parametersNotCorrect(
+                description: "'method' must be either 'GET', 'POST', 'PUT', 'DELETE', 'PATCH' or 'HEAD'")
+        }
+        return ResourceRequestFactory.makeResourceRequest(
+            path: path, method: method, parameters: details["parameters"] as? [String: Any], body: nil,
+            headers: details["headers"] as? [String: String], parametersEncoding: .formURL)
     }
 }
