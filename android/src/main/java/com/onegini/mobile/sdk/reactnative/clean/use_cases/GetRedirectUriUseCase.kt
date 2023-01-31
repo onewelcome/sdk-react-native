807c8898390d33bc66c20af214bf211bea9037ee
Michiel Vrins <michiel_678@hotmail.com>
2023-01-31 15:11:25 +0100

    RNP-143: Android: return string from getRedirectUri
    Previously we returned an object containing a string, this commit
    changes it to a direct String

--- android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/GetRedirectUriUseCase.kt
+++ android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/GetRedirectUriUseCase.kt
@@ -1,6 +1,5 @@
 package com.onegini.mobile.sdk.reactnative.clean.use_cases
 
-import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.onegini.mobile.sdk.reactnative.OneginiSDK
 import javax.inject.Inject
@@ -10,9 +9,6 @@ import javax.inject.Singleton
 class GetRedirectUriUseCase @Inject constructor(private val oneginiSDK: OneginiSDK) {
 
     operator fun invoke(promise: Promise) {
-        val uri = oneginiSDK.oneginiClient.configModel.redirectUri
-        val result = Arguments.createMap()
-        result.putString("redirectUri", uri)
-        promise.resolve(result)
+        promise.resolve(oneginiSDK.oneginiClient.configModel.redirectUri)
     }
 }
