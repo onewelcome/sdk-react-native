575fe04ba236bf27e8756e7f799803317eeb1db7
Michiel Vrins <michiel_678@hotmail.com>
2023-01-31 15:53:44 +0100

    RNP-142: Adjust setPreferredAuthenticator api + tests
    Previously we required the profileId to be passed in, but since we need
    to be authenticated anyway, we can just use the
    authenticatedUserProfile.

--- android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/SetPreferredAuthenticatorUseCase.kt
+++ android/src/main/java/com/onegini/mobile/sdk/reactnative/clean/use_cases/SetPreferredAuthenticatorUseCase.kt
@@ -1,6 +1,7 @@
 package com.onegini.mobile.sdk.reactnative.clean.use_cases
 
 import com.facebook.react.bridge.Promise
+import com.onegini.mobile.sdk.reactnative.OneginiSDK
 import com.onegini.mobile.sdk.reactnative.exception.OneginiReactNativeException
 import com.onegini.mobile.sdk.reactnative.managers.AuthenticatorManager
 import javax.inject.Inject
@@ -8,9 +9,9 @@ import javax.inject.Singleton
 
 @Singleton
 class SetPreferredAuthenticatorUseCase @Inject constructor(private val authenticatorManager: AuthenticatorManager) {
-    operator fun invoke(profileId: String, authenticatorId: String, promise: Promise) {
+    operator fun invoke(authenticatorId: String, promise: Promise) {
         try {
-            authenticatorManager.setPreferredAuthenticator(profileId, authenticatorId)
+            authenticatorManager.setPreferredAuthenticator(authenticatorId)
             promise.resolve(null)
         } catch (e: OneginiReactNativeException) {
             promise.reject(e.errorType.toString(), e.message)
