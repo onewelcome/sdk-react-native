ea525ef1a7ff80b86fe4dcb1571386eed2f3c2c4
Michiel Vrins <michiel_678@hotmail.com>
2023-02-08 16:52:48 +0100

    RNP-140: iOS: Verify body on certain request methods
    Some HTTP methods require or disallow a body, these cases are already
    forced by the type system on Android but this is not the case on iOS.
    To make the behavior consistent we just check them manually and throw
    the same error message.

--- ios/Handlers/ResourcesHandler.swift
+++ ios/Handlers/ResourcesHandler.swift
@@ -82,9 +82,9 @@ class ResourceHandler: BridgeToResourceHandlerProtocol {
                 description: "'method' must be either 'GET', 'POST', 'PUT', 'DELETE', 'PATCH' or 'HEAD'")
         }
         
-        guard let body = details["body"] as? String? else {
-            throw WrapperError.parametersNotCorrect(description: "Body must be of type String?")
-        }
+        let body = details["body"] as? String
+        
+        try verifyBodyAllowedWithHttpMethod(body, method)
         
         let headers = try getHeaders(details)
         return ResourceRequestFactory.makeResourceRequest(
@@ -93,6 +93,25 @@ class ResourceHandler: BridgeToResourceHandlerProtocol {
             body: body?.data(using: .utf8),
             headers: headers
         )
+    }
+    
+    private func verifyBodyAllowedWithHttpMethod(_ body: String?, _ method: HTTPMethod) throws {
+        switch (method) {
+        case .get:
+            if (body != nil && body != "") {
+                throw WrapperError.parametersNotCorrect(description: "Body is not allowed in GET request")
+            }
+        case .put:
+            if (body == nil || body == "") {
+                throw WrapperError.parametersNotCorrect(description: "Non-empty body is required in PUT request")
+            }
+        case .post:
+            if (body == nil || body == "") {
+                throw WrapperError.parametersNotCorrect(description: "Non-empty body is required in POST request")
+            }
+        default:
+            break;
+        }
     }
     
     private func getHeaders(_ details: Dictionary<String, Any?>) throws -> [String: String] {
