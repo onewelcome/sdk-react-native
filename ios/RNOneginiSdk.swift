8b26d9e50422fe42819d3f774a5283bb4ea57402
Michiel Vrins <michiel_678@hotmail.com>
2023-02-21 16:26:44 +0100

    RNP-151: iOS: Use Result class for authenticateUser completion

--- ios/RNOneginiSdk.swift
+++ ios/RNOneginiSdk.swift
@@ -268,9 +268,13 @@ class RNOneginiSdk: RCTEventEmitter, ConnectorToRNBridgeProtocol {
         let authenticators = userClient.authenticators(.all, for: profile)
         let authenticator = authenticators.first(where: {$0.identifier == authenticatorId}) ?? userClient.preferredAuthenticator
         bridgeConnector.toLoginHandler.authenticateUser(profile, authenticator: authenticator) {
-            (userProfile, customInfo, error) -> Void in
-            let result = mapToAuthData(profile: userProfile, info: customInfo)
-            self.resolveResultOrRejectError(resolve, reject, result, error)
+            (userProfile, result) -> Void in
+            switch result {
+            case .success(let response):
+                resolve(mapToAuthData(profile: userProfile, info: response))
+            case .failure(let error):
+                self.rejectWithError(reject, error)
+            }
         }
     }
 
